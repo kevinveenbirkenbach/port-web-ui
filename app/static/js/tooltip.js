861fd29d451473f230e4114ac5148f1947d66d74
Kevin Veen-Birkenbach <kevin@veen.world>
2025-01-08 20:32:00 +0100

    Added tooltip js

---
+++ app/static/js/tooltip.js
@@ -0,0 +1,7 @@
+// Initialisiert alle Tooltips auf der Seite
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
