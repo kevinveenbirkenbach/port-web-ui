562f5989e10b263bf06d1f5e3b17a7fc425176f8
Kevin Veen-Birkenbach <kevin@veen.world>
2025-01-09 15:17:34 +0100

    Solved identifier bug

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -1,18 +1,53 @@
 function openDynamicPopup(subitem) {
-    // Set modal title and content
-    document.getElementById('dynamicModalLabel').innerText = subitem.description;
-    const modalContent = document.getElementById('dynamicModalContent');
-    modalContent.value = subitem.identifier;
+  // Setze den Titel
+  document.getElementById('dynamicModalLabel').innerText = subitem.description;
 
-    // Add copy functionality
-    document.getElementById('dynamicCopyButton').addEventListener('click', function () {
+  // Setze den Identifier, falls vorhanden
+  const modalContent = document.getElementById('dynamicModalContent');
+  if (subitem.identifier) {
+      modalContent.value = subitem.identifier;
+  } else {
+      modalContent.value = '';
+  }
+
+  // Konfiguriere die Warnbox
+  const warningBox = document.getElementById('dynamicModalWarning');
+  if (subitem.warning) {
+      warningBox.classList.remove('d-none');
+      document.getElementById('dynamicModalWarningText').innerText = subitem.warning;
+  } else {
+      warningBox.classList.add('d-none');
+  }
+
+  // Konfiguriere die Infobox
+  const infoBox = document.getElementById('dynamicModalInfo');
+  if (subitem.info) {
+      infoBox.classList.remove('d-none');
+      document.getElementById('dynamicModalInfoText').innerText = subitem.info;
+  } else {
+      infoBox.classList.add('d-none');
+  }
+
+  // Konfiguriere den Link
+  const linkBox = document.getElementById('dynamicModalLink');
+  const linkHref = document.getElementById('dynamicModalLinkHref');
+  if (subitem.url) {
+      linkBox.classList.remove('d-none');
+      linkHref.href = subitem.url;
+  } else {
+      linkBox.classList.add('d-none');
+      linkHref.href = '#';
+  }
+
+  // Kopierfunktion für den Identifier
+  document.getElementById('dynamicCopyButton').addEventListener('click', function () {
       modalContent.select();
       navigator.clipboard.writeText(modalContent.value)
-        .then(() => alert('Content copied to clipboard!'))
-        .catch(() => alert('Failed to copy content.'));
-    });
+          .then(() => alert('Content copied to clipboard!'))
+          .catch(() => alert('Failed to copy content.'));
+  });
 
-    // Show the modal
-    const modal = new bootstrap.Modal(document.getElementById('dynamicModal'));
-    modal.show();
-  }
+  // Modal anzeigen
+  const modal = new bootstrap.Modal(document.getElementById('dynamicModal'));
+  modal.show();
+}
